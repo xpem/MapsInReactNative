653bcdcec9f8f96a8cfac8cbd89537121a45c8ce
emanuel <emanuel@enotas.com.br>
2021-07-09 10:22:56 -0300

    First version, work in progress, setting and testing locations

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import { css } from './assets/css/css';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+
+
+  useEffect(() => {
+    //invocation function
+    (async function () {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      //https://docs.expo.io/versions/latest/sdk/location/
+      let location = await Location.getCurrentPositionAsync({ enableHigthAccuracy: true });
+     // setLocation(location);
+      console.log(location);
+    })();
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={css.container}>
+      <MapView style={css.map}
+        initialRegion={{
+          //Localization
+          latitude: -19.93236901920649,
+          longitude: -43.938389827379524,
+          //
+          //zoom approximation
+          latitudeDelta: 0.00922,
+          longitudeDelta: 0.00421,
+          //
+        }}
+      >
+
+      </MapView>
+      <View style={css.search}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
